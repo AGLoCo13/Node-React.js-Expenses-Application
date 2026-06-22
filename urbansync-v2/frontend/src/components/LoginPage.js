50aa0b89e8a20543de80ce328dea30916048e014
StefanosTogias <35433576+Bleemoose@users.noreply.github.com>
2026-06-22 20:03:13 +0300

    FE change to test jenkins changes

--- urbansync-v2/frontend/src/components/LoginPage.js
+++ urbansync-v2/frontend/src/components/LoginPage.js
@@ -112,7 +112,7 @@ function LoginPage() {
               <p className="login-description">
                 Please login with your credentials to continue
               </p>
-              <span style={{ fontSize: '20px', color: '#cec36f' }}>v10 — CI/CD pipeline active with ArgoCD!</span>
+              <span style={{ fontSize: '20px', color: '#cec36f' }}>v11 — CI/CD pipeline active with ArgoCD!</span>
             </div>
 
             <form onSubmit={handleSubmit} className="login-form">
